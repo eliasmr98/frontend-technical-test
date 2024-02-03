e35b6479fbae272db44e11fb41ea77dc3be4dd62
eliasmr98 <eliasmartinrivas@gmail.com>
2024-02-03 18:27:09 +0100

    Add RickAndMortyService test

--- src/app/core/services/rick.and.morty.service.spec.ts
+++ src/app/core/services/rick.and.morty.service.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientModule } from '@angular/common/http';
 import { RickAndMortyService } from './rick.and.morty.service';
 
 describe('RickAndMortyService', () => {
   let service: RickAndMortyService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+    });
     service = TestBed.inject(RickAndMortyService);
   });
 
