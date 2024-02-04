aac47f2474077199900f74161e1c39e33add65f9
eliasmr98 <eliasmartinrivas@gmail.com>
2024-02-04 17:37:08 +0100

    Update List component test

--- src/app/components/list/list.component.spec.ts
+++ src/app/components/list/list.component.spec.ts
@@ -1,23 +1,76 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
-
+import { EMPTY, Observable, of, throwError } from 'rxjs';
 import { ListComponent } from './list.component';
+import { RickAndMortyService } from '../../core/services/rick.and.morty.service';
+import { ApiResponse } from '../../interfaces/api.response';
 
 describe('ListComponent', () => {
   let component: ListComponent;
   let fixture: ComponentFixture<ListComponent>;
+  let mockRickAndMortyService: jasmine.SpyObj<RickAndMortyService>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [HttpClientModule, ListComponent],
-    }).compileComponents();
+  beforeEach(() => {
+    mockRickAndMortyService = jasmine.createSpyObj('RickAndMortyService', [
+      'getCharacterList',
+    ]);
+
+    TestBed.configureTestingModule({
+      declarations: [],
+      imports: [HttpClientModule],
+      providers: [
+        { provide: RickAndMortyService, useValue: mockRickAndMortyService },
+      ],
+    });
 
     fixture = TestBed.createComponent(ListComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should handle errors on initialization', () => {
+    const errorMessage = 'Error fetching data';
+    mockRickAndMortyService.getCharacterList.and.returnValue(
+      new Observable((observer) => {
+        observer.error(errorMessage);
+      })
+    );
+
+    fixture.detectChanges();
+
+    expect(component.errorMessage).toBe(errorMessage);
+  });
+
+  it('should set characterResults$ on successful initialization', () => {
+    const apiResponse: ApiResponse = {
+      info: {
+        count: 2,
+        pages: 1,
+        next: null,
+        prev: null,
+      },
+      results: [
+        {
+          id: 1,
+          name: 'Rick Sanchez',
+          location: { name: 'Planet Earth', url: 'url.sample' },
+          status: 'alive',
+        },
+        {
+          id: 2,
+          name: 'Morty Smith',
+          location: { name: 'Planet Earth', url: 'url.sample' },
+          status: 'alive',
+        },
+      ],
+    };
+    mockRickAndMortyService.getCharacterList.and.returnValue(of(apiResponse));
+
+    fixture.detectChanges();
+
+    expect(component.characterResults$).toBeDefined();
   });
 });
