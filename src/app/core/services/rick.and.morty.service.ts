a64c37c1894daeec9f8fa2ff18137f06f8e53c57
eliasmr98 <eliasmartinrivas@gmail.com>
2024-02-04 16:34:57 +0100

    Refactor error handling with an interceptor

--- src/app/core/services/rick.and.morty.service.ts
+++ src/app/core/services/rick.and.morty.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, catchError, throwError } from 'rxjs';
+import { Observable } from 'rxjs';
 import { ApiResponse } from '../../interfaces/api.response';
 
 @Injectable({
@@ -10,20 +10,8 @@ export class RickAndMortyService {
   constructor(private http: HttpClient) {}
 
   getCharacterList(): Observable<ApiResponse> {
-    return this.http
-      .get<ApiResponse>('https://rickandmortyapi.com/apiadad/character/?page=1')
-      .pipe(
-        catchError((error: HttpErrorResponse) => {
-          let errorMessage = '';
-
-          if (error.error instanceof ErrorEvent) {
-            errorMessage = `Error: ${error.error.message}`;
-          } else {
-            errorMessage = `Error code: ${error.status}, message: ${error.message}`;
-          }
-
-          return throwError(() => errorMessage);
-        })
-      );
+    return this.http.get<ApiResponse>(
+      'https://rickandmortyapi.com/api/character/?page=1'
+    );
   }
 }
