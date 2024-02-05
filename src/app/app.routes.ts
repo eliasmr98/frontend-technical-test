e62cb56c6e8fefafe650b68376db7b8060f38568
eliasmr98 <eliasmartinrivas@gmail.com>
2024-02-05 20:35:41 +0100

    Add Error page component and its test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { ListComponent } from './components/list/list.component';
 import { TableComponent } from './components/table/table.component';
+import { ErrorPageComponent } from './components/error-page/error-page.component';
 
 export const routes: Routes = [
   { path: '', component: ListComponent },
   { path: 'table', component: TableComponent },
+  { path: '**', component: ErrorPageComponent },
 ];
