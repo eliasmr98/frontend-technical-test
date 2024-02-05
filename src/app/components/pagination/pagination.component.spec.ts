f7a9ede3dc869621c39623c1195ea9c09370b687
eliasmr98 <eliasmartinrivas@gmail.com>
2024-02-05 19:26:17 +0100

    Add Pagination component test

--- src/app/components/pagination/pagination.component.spec.ts
+++ src/app/components/pagination/pagination.component.spec.ts
@@ -1,16 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PaginationComponent } from './pagination.component';
 
 describe('PaginationComponent', () => {
   let component: PaginationComponent;
   let fixture: ComponentFixture<PaginationComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [PaginationComponent]
-    })
-    .compileComponents();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [PaginationComponent],
+    });
 
     fixture = TestBed.createComponent(PaginationComponent);
     component = fixture.componentInstance;
@@ -19,5 +17,17 @@ describe('PaginationComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit next event on nextPage()', () => {
+    spyOn(component.next, 'emit');
+    component.nextPage();
+    expect(component.next.emit).toHaveBeenCalled();
+  });
+
+  it('should emit prev event on prevPage()', () => {
+    spyOn(component.prev, 'emit');
+    component.prevPage();
+    expect(component.prev.emit).toHaveBeenCalled();
   });
 });
