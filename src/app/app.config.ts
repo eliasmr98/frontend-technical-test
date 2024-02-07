bcb85ee526fd2b308045ca90a308813775ae35fe
eliasmr98 <eliasmartinrivas@gmail.com>
2024-02-08 00:17:44 +0100

    Remove unused imports

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,8 +5,6 @@ import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { errorHandlerInterceptor } from './core/interceptors/error-handler.interceptor';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
 
 export const appConfig: ApplicationConfig = {
   providers: [
